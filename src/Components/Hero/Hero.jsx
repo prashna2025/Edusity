51a16af6d8b02a370c4296db41b5a487f4862d8a
prashna2025 <prashnaraut32@gmail.com>
2026-06-01 15:22:38 +0545

    5th

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,14 @@
+import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
+
 const Hero = () => {
     return (
         <div className='hero container'>
             <div className="hero-text">
                 <h1>We Ensure Quality Education for a Better Future</h1>
                 <p>Join us to unlock your potential,knowledge and experience and achieve your dreams with our comprehensive educational programs.</p>
-                <button className="btn">Explore More</button>
+                <button className="btn">Explore More <img src={dark_arrow} alt="" /></button>
             </div>
         </div>
     )
