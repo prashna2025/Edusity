62b2cd6bf0ea220ad8d9c6a2e0b3390ca692190c
prashna2025 <prashnaraut32@gmail.com>
2026-06-02 12:06:06 +0545

    8th

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,8 +8,8 @@ const About = () => {
     return (
         <div className='about'>
             <div className="about-left">
-                <img src={about_img} alt="About Us" />
-                <img src={play_icon} alt="Play Icon" />
+                <img src={about_img} alt="About Us" className="about-img" />
+                <img src={play_icon} alt="Play Icon" className="play-icon" />
 
             </div>
             <div className="about-right">
@@ -17,7 +17,7 @@ const About = () => {
                 <h2>Learn More About Our Institution</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim ullam eaque sapiente libero voluptatum dolor, aut est aspernatur consectetur provident cum quam mollitia voluptatem similique debitis modi non inventore molestiae commodi recusandae et rerum quibusdam facere. Itaque dolorum ducimus labore quod adipisci eaque consequatur sit non cumque ipsa quas, voluptatibus nesciunt corrupti perferendis reiciendis exercitationem accusamus autem provident similique earum fuga explicabo a voluptates sunt. Error explicabo dignissimos obcaecati rerum..</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <P>Our university is committed to providing a supportive and inclusive environment for all students. We offer a wide range of programs and resources to help you succeed academically and personally.</P>
+                <p>Our university is committed to providing a supportive and inclusive environment for all students. We offer a wide range of programs and resources to help you succeed academically and personally.</p>
             </div>
 
         </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
 
 const Navbar = () => {
+
+  const [sticky, setsticky] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 50 ? setsticky(true) : setsticky(false);
+
+    })
+
+
+  }, []);
+
   return (
-    <nav className='container'>
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
         <li>Home</li>
