7b385d8472d8dda56f91f2fbe1c94f45951d2f65
prashna2025 <prashnaraut32@gmail.com>
2026-06-02 16:04:40 +0545

    15th

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
 
 const Contact = () => {
   return (
     <div className="Contact">
       <div className="contact-col">
-        <h3>Send Message</h3>
+        <h3>Send Message <img src={msg_icon} alt="" /></h3>
         <p>Have any questions or inquiries? Feel free to reach out to us! We are here to assist you and provide the information you need. Whether you want to learn more about our programs, campus life, or anything else, don't hesitate to contact us. We look forward to hearing from you!</p>
         <ul>
             <li>Contact @learninghub26@gmail.com </li>
-             <li>02598453 </li>
-            <li>Location: Itahari, Nepal </li>
+             <li>02598453, +977-9845375424 </li>
+            <li>Location: Itahari-4 Halgada,<br/> Sunsari, Nepal </li>
 
         </ul>
       </div>
--- src/Components/Testimonials/Testimonial.jsx
+++ src/Components/Testimonials/Testimonial.jsx
@@ -28,68 +28,67 @@ const Testimonial = () => {
         slider.current.style.transform = `translateX(${tx}%)`;
     }
 
-}
-return (
-    <div className='testimonials'>
-        <img src={back_icon} alt="" className='back-btn' onClick={slideForward} />
-        <img src={next_icon} alt="" className='next-btn' onClick={slideBackward} />
-        <div className="slider">
-            <ul ref={slider}>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_1} alt="" />
-                            <div>
-                                <h3>Saraswoti Koirala</h3>
-                                <span>Educity,USA</span>
+    return (
+        <div className='testimonials'>
+            <img src={back_icon} alt="" className='back-btn' onClick={slideBackward} />
+            <img src={next_icon} alt="" className='next-btn' onClick={slideForward} />
+            <div className="slider">
+                <ul ref={slider}>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_1} alt="" />
+                                <div>
+                                    <h3>Saraswoti Koirala</h3>
+                                    <span>Educity,USA</span>
+                                </div>
                             </div>
+                            <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
                         </div>
-                        <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
-                    </div>
-                </li>
+                    </li>
 
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_2} alt="" />
-                            <div>
-                                <h3>John Doe</h3>
-                                <span>Educity,USA</span>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_2} alt="" />
+                                <div>
+                                    <h3>John Doe</h3>
+                                    <span>Educity,USA</span>
+                                </div>
                             </div>
+                            <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
                         </div>
-                        <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
-                    </div>
-                </li>
+                    </li>
 
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_3} alt="" />
-                            <div>
-                                <h3>Sarah Johnson</h3>
-                                <span>Educity,USA</span>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_3} alt="" />
+                                <div>
+                                    <h3>Sarah Johnson</h3>
+                                    <span>Educity,USA</span>
+                                </div>
                             </div>
+                            <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
                         </div>
-                        <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
-                    </div>
-                </li>
+                    </li>
 
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={user_4} alt="" />
-                            <div>
-                                <h3>Pratiksha Thapa</h3>
-                                <span>Educity,USA</span>
+                    <li>
+                        <div className="slide">
+                            <div className="user-info">
+                                <img src={user_4} alt="" />
+                                <div>
+                                    <h3>Pratiksha Thapa</h3>
+                                    <span>Educity,USA</span>
+                                </div>
                             </div>
+                            <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
                         </div>
-                        <p> "This is the best learning platform I have ever used. The courses are well-structured and the instructors are knowledgeable." </p>
-                    </div>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-)
- 
+    )
+}
 
 export default Testimonial
