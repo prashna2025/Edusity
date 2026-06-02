414a3561f8da17cca98964e19fd18a02113996b1
prashna2025 <prashnaraut32@gmail.com>
2026-06-02 15:42:17 +0545

    14

--- src/Components/Testimonials/Testimonial.jsx
+++ src/Components/Testimonials/Testimonial.jsx
@@ -7,11 +7,6 @@ import user_2 from '../../assets/user-2.png'
 import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
 
-
-
-
-
-
 const Testimonial = () => {
 
     const slider = useRef();
