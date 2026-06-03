475c788f2734b217d24af8c251387fd0c3dc6759
prashna2025 <prashnaraut32@gmail.com>
2026-06-03 15:26:46 +0545

    25th

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <div className='hero container'>
             <div className="hero-text">
-                <h1>We Ensure Quality Education for a Better Future</h1>
+                <h1>Creating a Foundation for Lifelong Success</h1>
                 <p>Join us to unlock your potential,knowledge and experience and achieve your dreams with our comprehensive educational programs.</p>
                 <button className="btn">Explore More <img src={dark_arrow} alt="" /></button>
             </div>
