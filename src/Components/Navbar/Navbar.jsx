960f67f05102ffc4a531e19c17c40aee5b6fb43d
prashna2025 <prashnaraut32@gmail.com>
2026-06-03 12:35:16 +0545

    19th

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
-import menu_icon from '../.../assets/menu-icon.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
@@ -16,14 +16,17 @@ const Navbar = () => {
 
   }, []);
 
-  const toggleMenu=
+  const[mobileMenu, setMobile] = useState(false);
+  const toggleMenu = () =>{
+    mobileMenu ? setMobile(false) : setMobile(true);
+  }
 
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <div className="brand-wrap">
         <span className="brand-name">Learninghub</span>
       </div>
-      <ul>
+      <ul className={mobileMenu? '':'hide-mobile-menu'}>
         <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
